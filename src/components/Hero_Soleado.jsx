d0c2f2176c9d294796b6ad8cb3c130dca88033bb
pacecapi <pacecapi@users.noreply.github.com>
2026-02-05 22:42:41 +0100

    Fix: page soleado a la izquierda

--- src/components/Hero_Soleado.jsx
+++ src/components/Hero_Soleado.jsx
@@ -7,7 +7,8 @@ const Hero_Soleado = () => {
             <div className="hero-soleado__content container">
                 <div className="hero-soleado__text">
                     <h1 className="hero-soleado__title">
-                        ZC Group: Innovating<br />
+                        ZC Group:<br />
+                        Innovating<br />
                         for a Cleaner Future.
                     </h1>
                     <p className="hero-soleado__description">
